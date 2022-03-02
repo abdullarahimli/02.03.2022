e09af1cc1a429673881d27e9d3a76b1b7b651f3b
abdullarahimli <abdullavr@code.edu.az>
2022-03-02 11:18:50 +0400

    First Commit

---
+++ Task1/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    class Book
+    {
+        private static int _count = 1;
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+
+        public Book(string name, string authorName, int pageCount)
+        {
+            _count++;
+            Name = name;
+            AuthorName = authorName;
+            PageCount = pageCount;
+            Code = $"{Name[0..2].ToUpper()}{_count}";
+        }
+    }
+}
---
+++ Task1/Library.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace Task1
+{
+    class Library
+    {
+
+
+        List<Book> Books = new List<Book>();
+        
+        
+        public string FindAllBooksByName(string bookNameFind)
+        {
+            
+        }
+        public string RemoveAllBookByName(string bookNameRemove)
+        {
+
+        }
+        public string SearchBoos(string bookNameSearch)
+        {
+
+        }
+        public int FindAllBookByPageCountRange(int one, int two)
+        {
+
+        }
+        public string RemoveByNo(string no)
+        {
+
+        }
+    }
+}
